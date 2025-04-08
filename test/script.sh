3cac381cea02577b779930a23f3cdcee783b4b80
Anska <103422661+algadea@users.noreply.github.com>
2025-04-08 04:44:04 +0200

    add map and test directories

---
+++ test/script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Global Variables
+PROJECT_DIR=(cd .. && pwd)
+TEST_DIR=$(pwd)
+BINARY=cub3d
+
+TEST_CASE=(
+	"empty-map.cub"
+	"not-closed.cub"
+)
+
+print_header() {
+	echo cacahuete
+}
+
+main() {
+	print_header
+}
