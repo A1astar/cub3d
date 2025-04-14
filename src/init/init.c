9931b33f8a89086fb4e55f81f14f5d41b3650901
Anska <103422661+algadea@users.noreply.github.com>
2025-04-14 15:51:17 +0200

    update files

--- src/init/init.c
+++ src/init/init.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   init.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alacroix <alacroix@student.42.fr>          +#+  +:+       +#+        */
+/*   By: algadea <algadea@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 11:27:44 by algadea           #+#    #+#             */
-/*   Updated: 2025/04/14 15:36:44 by alacroix         ###   ########.fr       */
+/*   Updated: 2025/04/14 15:50:40 by algadea          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,18 +36,24 @@ void	init_struct_attributes(t_cub3d *cub3d)
 void	init_program(t_cub3d *cub3d, char **argv)
 {
 	init_struct_attributes(cub3d);
-	int	map_width = (cub3d->minimap.width + cub3d->minimap.x_origin) * cub3d->minimap.tile_width;
-	int	map_height = (cub3d->minimap.height + cub3d->minimap.y_origin) * cub3d->minimap.tile_height;
 	parsing(cub3d, argv[1]);
 	init_mlx(cub3d, &cub3d->window);
 	init_asset(cub3d);
-	cub3d->minimap.x_origin = WINDOW_WIDTH * 0.72;
-	if (map_width > WINDOW_WIDTH)
-		cub3d->minimap.x_origin -= WINDOW_WIDTH - map_width;
-	if (map_height > WINDOW_HEIGHT)
-		cub3d->minimap.y_origin -= WINDOW_HEIGHT - map_height;
-	cub3d->minimap.y_origin = WINDOW_HEIGHT * 0.8;
+
+
+
+
 	cub3d->minimap.tile_height = 5;
 	cub3d->minimap.tile_width = 5;
 	cub3d->minimap.scale = 1;
+
+	cub3d->minimap.x_origin = WINDOW_WIDTH * 0.66;
+	int	map_width = cub3d->minimap.width * cub3d->minimap.tile_width;
+	if (map_width + cub3d->minimap.x_origin > WINDOW_WIDTH)
+		cub3d->minimap.x_origin =  WINDOW_WIDTH - map_width;
+
+	cub3d->minimap.y_origin = WINDOW_HEIGHT * 0.8;
+	int	map_height = cub3d->minimap.height * cub3d->minimap.tile_height;
+	if (map_height + cub3d->minimap.y_origin > WINDOW_HEIGHT)
+		cub3d->minimap.y_origin = WINDOW_HEIGHT - map_height;
 }
