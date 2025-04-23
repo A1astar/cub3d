11ee78616a78dffbe63683876db67bbd07a90d14
Anska <103422661+algadea@users.noreply.github.com>
2025-04-23 18:00:08 +0200

    update files

--- src/physics/hitbox_player.c
+++ src/physics/hitbox_player.c
@@ -44,14 +44,14 @@ bool	can_move_to_south(t_raycast *raycast, t_player *player, t_map *map)
 
 bool	can_move_to_west(t_raycast *raycast, t_player *player, t_map *map)
 {
-	if (map->map[(int)(player->y_pos + raycast->y_dir
+	if (map->map[(int)(player->y_pos - raycast->x_dir
 			* VELOCITY)][(int)player->x_pos] == '1'
-		|| map->map[(int)player->y_pos][(int)(player->x_pos + raycast->x_dir
+		|| map->map[(int)player->y_pos][(int)(player->x_pos + raycast->y_dir
 			* VELOCITY)] == '1')
 		return (false);
-	else if (map->map[(int)(player->y_pos + raycast->y_dir
+	else if (map->map[(int)(player->y_pos - raycast->x_dir
 			* VELOCITY)][(int)player->x_pos] == 'C'
-		|| map->map[(int)player->y_pos][(int)(player->x_pos + raycast->x_dir
+		|| map->map[(int)player->y_pos][(int)(player->x_pos + raycast->y_dir
 			* VELOCITY)] == 'C')
 		return (false);
 	return (true);
@@ -59,14 +59,14 @@ bool	can_move_to_west(t_raycast *raycast, t_player *player, t_map *map)
 
 bool	can_move_to_east(t_raycast *raycast, t_player *player, t_map *map)
 {
-	if (map->map[(int)(player->y_pos - raycast->y_dir
+	if (map->map[(int)(player->y_pos + raycast->x_dir
 			* VELOCITY)][(int)player->x_pos] == '1'
-		|| map->map[(int)player->y_pos][(int)(player->x_pos - raycast->x_dir
+		|| map->map[(int)player->y_pos][(int)(player->x_pos - raycast->y_dir
 			* VELOCITY)] == '1')
 		return (false);
-	else if (map->map[(int)(player->y_pos - raycast->y_dir
+	else if (map->map[(int)(player->y_pos + raycast->x_dir
 			* VELOCITY)][(int)player->x_pos] == 'C'
-		|| map->map[(int)player->y_pos][(int)(player->x_pos - raycast->x_dir
+		|| map->map[(int)player->y_pos][(int)(player->x_pos - raycast->y_dir
 			* VELOCITY)] == 'C')
 		return (false);
 	return (true);
