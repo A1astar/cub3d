1e5e49edbed33bbc59df7a2f524802c010634b42
Anska <103422661+algadea@users.noreply.github.com>
2025-04-23 16:35:27 +0200

    update files

--- src/event/event_player_movement.c
+++ src/event/event_player_movement.c
@@ -6,7 +6,7 @@
 /*   By: algadea <algadea@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 15:24:39 by algadea           #+#    #+#             */
-/*   Updated: 2025/04/23 16:00:49 by algadea          ###   ########.fr       */
+/*   Updated: 2025/04/23 16:34:26 by algadea          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,17 +25,20 @@ void	move_player_east(t_cub3d *cub3d)
 	if (!can_move_to_east(&cub3d->raycast, &cub3d->player, &cub3d->map))
 		return ;
 	printf("x_dir = %f | y_dir = %f\n", cub3d->raycast.x_dir, cub3d->raycast.y_dir);
-	if (cub3d->raycast.x_dir > 0)
-	cub3d->player.x_pos += cub3d->raycast.x_dir * VELOCITY;
-	cub3d->player.y_pos += cub3d->raycast.y_dir * VELOCITY;
+	if (cub3d->raycast.x_dir > 0 && cub3d->player.x_pos)
+		cub3d->player.y_pos += cub3d->raycast.y_dir * VELOCITY;
+	else if (cub3d->raycast.x_dir < 0)
+		cub3d->player.y_pos -= cub3d->raycast.y_dir * VELOCITY;
 }
 
 void	move_player_west(t_cub3d *cub3d)
 {
 	if (!can_move_to_west(&cub3d->raycast, &cub3d->player, &cub3d->map))
 		return ;
-	cub3d->player.x_pos -= cub3d->raycast.x_dir * VELOCITY;
-	cub3d->player.y_pos -= cub3d->raycast.y_dir * VELOCITY;
+	if (cub3d->raycast.x_dir > 0)
+		cub3d->player.y_pos -= cub3d->raycast.y_dir * VELOCITY;
+	else if (cub3d->raycast.x_dir < 0)
+		cub3d->player.y_pos += cub3d->raycast.y_dir * VELOCITY;
 }
 
 void	move_player_south(t_cub3d *cub3d)
