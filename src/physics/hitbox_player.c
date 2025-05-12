c41376c9ea7bf48f4a78db3ffecff584f3b27e7f
Anska <103422661+algadea@users.noreply.github.com>
2025-05-12 16:41:07 +0200

    update files

--- src/physics/hitbox_player.c
+++ src/physics/hitbox_player.c
@@ -3,82 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   hitbox_player.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alacroix <alacroix@student.42.fr>          +#+  +:+       +#+        */
+/*   By: algadea <algadea@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/07 11:58:09 by algadea           #+#    #+#             */
-/*   Updated: 2025/05/11 17:45:14 by alacroix         ###   ########.fr       */
+/*   Updated: 2025/05/12 16:31:54 by algadea          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/cub3d.h"
-
-// bool	can_move_to_north(t_time *time, t_raycast *raycast,
-// 			t_player *player, t_map *map)
-// {
-// 	(void)time;
-// 	if (map->map[(int)(player->y_pos + raycast->y_dir
-// 			* player->velocity)][(int)player->x_pos] == '1'
-// 		|| map->map[(int)player->y_pos][(int)(player->x_pos + raycast->x_dir
-// 			* player->velocity)] == '1')
-// 		return (false);
-// 	else if (map->map[(int)(player->y_pos + raycast->y_dir
-// 			* player->velocity)][(int)player->x_pos] == 'C'
-// 		|| map->map[(int)player->y_pos][(int)(player->x_pos + raycast->x_dir
-// 			* player->velocity)] == 'C')
-// 		return (false);
-// 	return (true);
-// }
-
-// bool	can_move_to_south(t_time *time, t_raycast *raycast,
-// 			t_player *player, t_map *map)
-// {
-// 	(void)time;
-// 	if (map->map[(int)(player->y_pos - raycast->y_dir
-// 			* player->velocity)][(int)player->x_pos] == '1'
-// 		|| map->map[(int)player->y_pos][(int)(player->x_pos - raycast->x_dir
-// 			* player->velocity)] == '1')
-// 		return (false);
-// 	else if (map->map[(int)(player->y_pos - raycast->y_dir
-// 			* player->velocity)][(int)player->x_pos] == 'C'
-// 		|| map->map[(int)player->y_pos][(int)(player->x_pos - raycast->x_dir
-// 			* player->velocity)] == 'C')
-// 		return (false);
-// 	return (true);
-// }
-
-// bool	can_move_to_west(t_time *time, t_raycast *raycast,
-// 			t_player *player, t_map *map)
-// {
-// 	(void)time;
-// 	if (map->map[(int)(player->y_pos - raycast->x_dir
-// 			* player->velocity)][(int)player->x_pos] == '1'
-// 		|| map->map[(int)player->y_pos][(int)(player->x_pos + raycast->y_dir
-// 			* player->velocity)] == '1')
-// 		return (false);
-// 	else if (map->map[(int)(player->y_pos - raycast->x_dir
-// 			* player->velocity)][(int)player->x_pos] == 'C'
-// 		|| map->map[(int)player->y_pos][(int)(player->x_pos + raycast->y_dir
-// 			* player->velocity)] == 'C')
-// 		return (false);
-// 	return (true);
-// }
-
-// bool	can_move_to_east(t_time *time, t_raycast *raycast,
-// 			t_player *player, t_map *map)
-// {
-// 	(void)time;
-// 	if (map->map[(int)(player->y_pos + raycast->x_dir
-// 			* player->velocity)][(int)player->x_pos] == '1'
-// 		|| map->map[(int)player->y_pos][(int)(player->x_pos - raycast->y_dir
-// 			* player->velocity)] == '1')
-// 		return (false);
-// 	else if (map->map[(int)(player->y_pos + raycast->x_dir
-// 			* player->velocity)][(int)player->x_pos] == 'C'
-// 		|| map->map[(int)player->y_pos][(int)(player->x_pos - raycast->y_dir
-// 			* player->velocity)] == 'C')
-// 		return (false);
-// 	return (true);
-// }
 
 bool	entity_collision(double x_origin, double y_origin, double target_x, double target_y)
 {
